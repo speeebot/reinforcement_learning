f1df9eb963708150fa7f0efaaa0c4690d1595628
speeebot <speeebot@gmail.com>
2022-11-05 02:28:12 -0400

    initial commit. boilerplate

---
+++ main.py
@@ -0,0 +1,278 @@
+"""
+    This code communicated with the coppeliaSim simulation and simulates pouring aiming to a destination container
+    
+    WINDOWS:
+    ./Applications/coppeliaSim.app/Contents/MacOSMacOS/coppeliaSim -gREMOTEAPISERVERSERVICE_19000_FALSE_TRUE ~/path/to/file/2cups_Intro_to_AI.ttt
+
+    MACOS:
+    cd ~/Applications/coppeliaSim.app/Contents/MacOS
+    ./coppeliaSim -gREMOTEAPISERVERSERVICE_19000_FALSE_TRUE ~/Documents/school/fall22/intro_ai/project2_part1/2cups_Intro_to_AI.ttt
+
+"""
+
+import sys
+
+sys.path.append('MacAPI')
+import numpy as np
+import sim
+
+# Max movement along X
+low, high = -0.05, 0.05
+
+
+def setNumberOfBlocks(clientID, blocks, typeOf, mass, blockLength,
+                      frictionCube, frictionCup):
+    '''
+        Function to set the number of blocks in the simulation
+        '''
+    emptyBuff = bytearray()
+    res, retInts, retFloats, retStrings, retBuffer = sim.simxCallScriptFunction(
+        clientID, 'Table', sim.sim_scripttype_childscript, 'setNumberOfBlocks',
+        [blocks], [mass, blockLength, frictionCube, frictionCup], [typeOf],
+        emptyBuff, sim.simx_opmode_blocking)
+    if res == sim.simx_return_ok:
+        print(
+            'Results: ', retStrings
+        )  # display the reply from CoppeliaSim (in this case, the handle of the created dummy)
+    else:
+        print('Remote function call failed')
+
+
+def triggerSim(clientID):
+    e = sim.simxSynchronousTrigger(clientID)
+    step_status = 'successful' if e == 0 else 'error'
+    # print(f'Finished Step {step_status}')
+
+
+def rotation_velocity(rng):
+    ''' Set rotation velocity randomly, rotation velocity is a composition of two sinusoidal velocities '''
+    #Sinusoidal velocity
+    forward = [-0.3, -0.35, -0.4]
+    backward = [0.75, 0.8, 0.85]
+    freq = 60
+    ts = np.linspace(0, 1000 / freq, 1000)
+    velFor = rng.choice(forward) * np.sin(2 * np.pi * 1 / 20 * ts)
+    velBack = rng.choice(backward) * np.sin(2 * np.pi * 1 / 10 * ts)
+    velSin = velFor
+    idxFor = np.argmax(velFor > 0)
+    velSin[idxFor:] = velBack[idxFor:]
+    velReal = velSin
+    return velReal
+
+
+def start_simulation():
+    ''' Function to communicate with Coppelia Remote API and start the simulation '''
+    sim.simxFinish(-1)  # just in case, close all opened connections
+    clientID = sim.simxStart('127.0.0.1', 19000, True, True, 5000,
+                             5)  # Connect to CoppeliaSim
+    if clientID != -1:
+        print('Connected to remote API server')
+    else:
+        print("fail")
+        sys.exit()
+
+    returnCode = sim.simxSynchronous(clientID, True)
+    returnCode = sim.simxStartSimulation(clientID, sim.simx_opmode_blocking)
+
+    if returnCode != 0 and returnCode != 1:
+        print("something is wrong")
+        print(returnCode)
+        exit(0)
+
+    triggerSim(clientID)
+
+    # get the handle for the source container
+    res, pour = sim.simxGetObjectHandle(clientID, 'joint',
+                                        sim.simx_opmode_blocking)
+    res, receive = sim.simxGetObjectHandle(clientID, 'receive',
+                                           sim.simx_opmode_blocking)
+    # start streaming the data
+    returnCode, original_position = sim.simxGetObjectPosition(
+        clientID, pour, -1, sim.simx_opmode_streaming)
+    returnCode, original_position = sim.simxGetObjectPosition(
+        clientID, receive, -1, sim.simx_opmode_streaming)
+    returnCode, original_position = sim.simxGetJointPosition(
+        clientID, pour, sim.simx_opmode_streaming)
+
+    return clientID, pour, receive
+
+
+def stop_simulation(clientID):
+    ''' Function to stop the episode '''
+    sim.simxStopSimulation(clientID, sim.simx_opmode_blocking)
+    sim.simxFinish(clientID)
+
+
+def get_object_handles(clientID, pour):
+    # Drop blocks in source container
+    triggerSim(clientID)
+    number_of_blocks = 2
+    print('Initial number of blocks=', number_of_blocks)
+    setNumberOfBlocks(clientID,
+                      blocks=number_of_blocks,
+                      typeOf='cube',
+                      mass=0.002,
+                      blockLength=0.025,
+                      frictionCube=0.06,
+                      frictionCup=0.8)
+    triggerSim(clientID)
+
+    # Get handles of cubes created
+    object_shapes_handles = []
+    obj_type = "Cuboid"
+    for obj_idx in range(number_of_blocks):
+        res, obj_handle = sim.simxGetObjectHandle(clientID,
+                                                  f'{obj_type}{obj_idx}',
+                                                  sim.simx_opmode_blocking)
+        object_shapes_handles.append(obj_handle)
+
+    triggerSim(clientID)
+
+    for obj_handle in object_shapes_handles:
+        # get the starting position of source
+        returnCode, obj_position = sim.simxGetObjectPosition(
+            clientID, obj_handle, -1, sim.simx_opmode_streaming)
+
+    returnCode, position = sim.simxGetJointPosition(clientID, pour,
+                                                    sim.simx_opmode_buffer)
+    returnCode, obj_position = sim.simxGetObjectPosition(
+        clientID, pour, -1, sim.simx_opmode_buffer)
+    print(f'Pouring Cup Initial Position:{obj_position}')
+    # Give time for the cubes to finish falling
+    _wait(clientID)
+    return object_shapes_handles, obj_position
+
+
+def set_cup_initial_position(clientID, pour, receive, cup_position, rng):
+
+    # Move cup along x axis
+    global low, high
+    move_x = low + (high - low) * rng.random()
+    cup_position[0] = cup_position[0] + move_x
+
+    returnCode = sim.simxSetObjectPosition(clientID, pour, -1, cup_position,
+                                           sim.simx_opmode_blocking)
+    triggerSim(clientID)
+    returnCode, pour_position = sim.simxGetObjectPosition(
+        clientID, pour, -1, sim.simx_opmode_buffer)
+    print(f'Pouring Cup Moved Position:{cup_position}')
+    returnCode, receive_position = sim.simxGetObjectPosition(
+        clientID, receive, -1, sim.simx_opmode_buffer)
+    print(f'Receiving Cup Position:{cup_position}')
+    return pour_position, receive_position
+
+
+def get_state(object_shapes_handles, clientID, pour, j):
+    ''' Function to get the cubes and pouring cup position '''
+
+    # Get position of the objects
+    obj_pos = []
+    for obj_handle in object_shapes_handles:
+        # get the starting position of source
+        returnCode, obj_position = sim.simxGetObjectPosition(
+            clientID, obj_handle, -1, sim.simx_opmode_buffer)
+        obj_pos.append(obj_position)
+
+    returnCode, cup_position = sim.simxGetObjectPosition(
+        clientID, pour, -1, sim.simx_opmode_buffer)
+
+    return obj_pos, cup_position
+
+
+def move_cup(clientID, pour, action, cup_position, center_position):
+    ''' Function to move the pouring cup laterally during the rotation '''
+
+    global low, high
+    resolution = 0.001
+    move_x = resolution * action
+    movement = cup_position[0] + move_x
+    if center_position + low < movement < center_position + high:
+        cup_position[0] = movement
+        returnCode = sim.simxSetObjectPosition(clientID, pour, -1,
+                                               cup_position,
+                                               sim.simx_opmode_blocking)
+
+
+def rotate_cup(clientID, speed, pour):
+    ''' Function to rotate cup '''
+    errorCode = sim.simxSetJointTargetVelocity(clientID, pour, speed,
+                                               sim.simx_opmode_oneshot)
+    returnCode, position = sim.simxGetJointPosition(clientID, pour,
+                                                    sim.simx_opmode_buffer)
+    return position
+
+
+def _wait(clientID):
+    for _ in range(60):
+        triggerSim(clientID)
+
+
+def main():
+
+    rng = np.random.default_rng()
+    # Set rotation velocity randomly
+    velReal = rotation_velocity(rng)
+
+    # Start simulation
+    clientID, pour, receive = start_simulation()
+    object_shapes_handles, cup_position = get_object_handles(clientID, pour)
+
+    # Get initial position of the cups
+    cup_position, receive_position = set_cup_initial_position(
+        clientID, pour, receive, cup_position, rng)
+    _wait(clientID)
+    center_position = cup_position[0]
+
+    for j in range(velReal.shape[0]):
+        # 60HZ
+        triggerSim(clientID)
+        # Make sure simulation step finishes
+        returnCode, pingTime = sim.simxGetPingTime(clientID)
+
+        # Get current state
+        cubes_position, cup_position = get_state(object_shapes_handles,
+                                                 clientID, pour, j)
+
+        print(f"Step {j}, Cubes position: {cubes_position}, Cup position: {cup_position}")
+
+        # Rotate cup
+        speed = velReal[j]
+
+        position = rotate_cup(clientID, speed, pour)
+
+        #position = 1
+        # call rotate_cup function and assign the return value to position variable
+        # it will be something like position = rotate_cup()
+
+        # Move cup laterally
+        actions = [-2, -1, 0, 1, 2]
+
+        choice = np.random.choice(actions)
+
+        '''if receive_position[0] > cup_position[0]:
+            move_cup(clientID, pour, actions[4], cup_position, center_position)
+        elif receive_position[0] < cup_position[0]:
+            move_cup(clientID, pour, actions[0], cup_position, center_position)
+        else:
+            pass'''
+
+        move_cup(clientID, pour, choice, cup_position, center_position)
+        
+
+        print(f"Action taken: {choice}")
+
+        # call move_cup function
+       # move_cup(clientID, pour, actions[4], cup_position, center_position)
+
+        # Break if cup goes back to vertical position
+        if j > 10 and position > 0:
+            break
+
+    print(f"Cubes final position: {cubes_position}")
+    # Stop simulation
+    stop_simulation(clientID)
+
+
+if __name__ == '__main__':
+
+    main()
